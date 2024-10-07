7b198b51b8ad9701457796da6946843a129b2e29
yanfang chen <juliechen209@gmail.com>
2024-10-07 13:20:13 -0500

    use Dapper and entity framework to interact with MS sql server

---
+++ Data/DataContextDapper.cs
@@ -0,0 +1,57 @@
+
+using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+/* 
+IDbConnection is an interface from the ADO.NET library  in the System.Data namespace that defines the basic functionality 
+required for a database connection. It provides a generic way to work with database connections, 
+allowing you to use various types of database connections (like SQL Server, Oracle, MySQL) 
+without being tied to a specific implementation.
+SqlConnection is a class that implements the IDbConnection interface
+*/
+namespace csharpstarterapp.Data {
+    public class DataContextDapper{
+        //location for database and credential
+        //private IConfiguration _config;
+        private string _connectionString;
+        public DataContextDapper(IConfiguration config) {
+           // _config = config;
+            _connectionString = config.GetConnectionString("DefaultConnection");
+            
+        }
+
+
+
+        public IEnumerable<T> LoadData<T>(string sql) {
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+            return dbConnection.Query<T>(sql);
+        }
+
+        public T LoadDataSingle<T>(string sql) {
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+            return dbConnection.QuerySingle<T>(sql);
+        }
+
+        public bool ExecuteSql(string sql) {
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+            return dbConnection.Execute(sql) > 0;
+        }
+        
+        public int ExecuteSqlWithRowCount(string sql) {
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+            return dbConnection.Execute(sql);
+        }
+
+
+
+        
+
+
+
+
+        
+
+    }
+}
---
+++ Models/Computer.cs
@@ -0,0 +1,30 @@
+namespace csharpstarterapp.Models{
+    public class Computer 
+    {
+        public int ComputerId {get; set;}
+        public string Motherboard {get; set;}
+        // a second method set value to null-set default value, instead of constructor
+        //public string Motherboard {get; set;} = "";
+        public int? CPUCores {get; set;}
+        public bool HasWifi {get; set;}
+        public Boolean HasLTE {get; set;}
+        public DateTime ReleaseDate {get; set;}
+        public decimal Price {get; set;}
+        public string VideoCard {get; set;}
+
+        public Computer() {
+            if (VideoCard == null) {
+                VideoCard = "";
+            }
+            if (Motherboard == null) {
+                Motherboard = "";
+            }
+            if (CPUCores == null) {
+                CPUCores = 0;
+            }
+        }
+
+
+        
+    }
+}
